40c1ed9163f735f5a2c53677b621362f7130d1d6
Zach Hancock <zhancock@edx.org>
2022-11-17 11:20:03 -0500

    add learner dash mfe

--- tutorhacks/plugin.py
+++ tutorhacks/plugin.py
@@ -12,6 +12,27 @@ mysql:
     )
 )
 
+# custom webpack for learner dash
+# jank just to get this working for now
+hooks.Filters.ENV_PATCHES.add_item(
+    (
+        "mfe-webpack-dev-config",
+        """
+const path = require('path');
+const config = module.exports
+
+config.resolve.modules = [
+  path.resolve(__dirname, './src'),
+  'node_modules',
+];
+
+config.module.rules[0].exclude = /node_modules\/(?!(query-string|split-on-first|strict-uri-encode|@edx))/;
+
+module.exports = config;
+"""
+    )
+)
+
 # Adds non-default apps to the tutor-mfe plugin
 hooks.Filters.CONFIG_DEFAULTS.add_items([
     (
@@ -22,9 +43,24 @@ hooks.Filters.CONFIG_DEFAULTS.add_items([
             "port": 2001,
             "env": {
                 "development": {
-                    "EXAMS_BASE_URL": "http://exams.local.overhang.io:8740"
+                    "EXAMS_BASE_URL": "http://exams.local.overhang.io:8740",
+                    "BASE_URL": "http://apps.local.overhang.io:2001"
                 }
             }
+        },
+    ),
+    (
+        "MFE_LEARNER_DASHBOARD_MFE_APP",
+        {
+            "name": "dashboard",
+            "repository": "https://github.com/edx/frontend-app-learner-dashboard",
+            "port": 1996,
+            "env": {
+                "development": {
+                    "BASE_URL": "http://apps.local.overhang.io:1996",
+                    "FAVICON_URL": "https://edx-cdn.org/v3/default/favicon.ico",
+                },
+            },
         },
     ),
 ])
